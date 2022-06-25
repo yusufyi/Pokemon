5760c46ef2e88d9d2550514c96c2fe26775c1777
yusufyi <yusufyi@hotmail.com>
2022-06-25 14:53:52 +0100

    test2

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcomde to <a href="https://nextjs.org">Next.js!</a>
+          Welcomdeto <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
